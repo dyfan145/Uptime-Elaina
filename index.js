f53c16eca92a28252a5a60e21b83bb2f42cac78c
dyfan145 <69150154+dyfan145@users.noreply.github.com>
2021-08-31 18:34:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["(your glitch link)"]
+const urls = ["https://scarce-three-stay.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
